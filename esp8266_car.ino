2466fb52db5983ae294718435910274245729f17
Henry <henryouly@gmail.com>
2017-04-16 11:31:44 -0700

    use ESP8266WebServer to simplify the handle logic; support pwm

--- esp8266_car.ino
+++ esp8266_car.ino
@@ -1,4 +1,6 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
 
 int FORWARD = 0;
 int BACKWARD = 1;
@@ -8,24 +10,59 @@ int STOP = 4;
 int PIN_DEF[][4] = {
   {HIGH, LOW, LOW, HIGH},
   {LOW, HIGH, HIGH, LOW},
-  {HIGH, LOW, HIGH, LOW},
-  {LOW, HIGH, LOW, HIGH},
+  {HIGH, LOW, HIGH, HIGH},
+  {HIGH, HIGH, LOW, HIGH},
   {HIGH, HIGH, HIGH, HIGH},
 };
 int* PIN = new int[4] { D3, D4, D5, D6 };
+int* PWM_PIN = new int[2] { D0, D2 };
+String WEBPAGE = 
+  "<h1>ESP8266 CAR</h1>"
+  "<a href=\"/forward\"><button>FORWARD</button></a>"
+  "<a href=\"/backward\"><button>BACKWARD</button></a>"
+  "<a href=\"/left\"><button>LEFT</button></a>"
+  "<a href=\"/right\"><button>RIGHT</button></a>"
+  "<a href=\"/stop\"><button>STOP</button></a>";
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
 
 void setup() {
   initHardware();
   setupWiFi();
+  setupWebServer();
+}
+
+void setupWebServer() {
+  server.on("/", []() {
+    server.send(200, "text/html", WEBPAGE);
+  });
+  server.on("/forward", []() { process(FORWARD); });
+  server.on("/backward", []() { process(BACKWARD); });
+  server.on("/left", []() { process(LEFT); });
+  server.on("/right", []() { process(RIGHT); });
+  server.on("/stop", []() { process(STOP); });
+  server.on("/pwm1", []() {
+    int pwm=server.arg("pwm").toInt();
+    analogWrite(PWM_PIN[0], pwm);
+    server.send(200, "text/html", WEBPAGE);    
+  });
+  server.on("pwm2", []() {
+    int pwm=server.arg("pwm").toInt();
+    analogWrite(PWM_PIN[1], pwm);
+    server.send(200, "text/html", WEBPAGE);    
+  });
   server.begin();
+  Serial.println("HTTP server started");
 }
 
 void initHardware() {
   Serial.begin(115200);
   for (int i = 0; i < 4; i++) 
     pinMode(PIN[i], OUTPUT);
+  for (int i = 0; i < 2; i++)
+    pinMode(PWM_PIN[i], OUTPUT);
+  analogWrite(PWM_PIN[0], 1023);
+  analogWrite(PWM_PIN[1], 1023);
 }
 
 void setupWiFi() {
@@ -37,35 +74,12 @@ void setupWiFi() {
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-
-  String req = client.readStringUntil('\r');
-  Serial.print("Request: ");
-  Serial.println(req);
-  client.flush();
-
-  processRequest(req);
-}
-
-void processRequest(String req) {
-  if (req.indexOf("forward") != -1) {
-    process(FORWARD);
-  } else if (req.indexOf("backward") != -1) {
-    process(BACKWARD);
-  } else if (req.indexOf("left") != -1) {
-    process(LEFT);
-  } else if (req.indexOf("right") != -1) {
-    process(RIGHT);
-  } else if (req.indexOf("stop") != -1) {
-    process(STOP);
-  }
+  server.handleClient();
 }
 
 void process(int cmd) {
   for (int i = 0; i < 4; i++)
     digitalWrite(PIN[i], PIN_DEF[cmd][i]);
+  server.send(200, "text/html", WEBPAGE);
 }
 
